d28dffcb312bcde78ef6b908468ca8e59bdf6705
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-03-01 16:59:19 +0100

    Admin::UsersController requires signed in admin user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,18 +1,21 @@
 class Admin::UsersController < ApplicationController
+
+	before_filter :authenticate_user!, :is_allowed
+
 	def index
-		if user_signed_in?
-			if current_user.is_admin
-				@users = User.all
-			else
-				flash[:alert] = "Page does not exist"
-				redirect_to root_path
-			end
-		end 	
+		@users = User.where.not(uuid: current_user.uuid)
 	end
 	
 	def edit
-		redirect_to root_path unless current_user.is_admin 
-		
-		@user = User.find_by_uuid(params[:id])
+		@user = User.where(uuid: params[:id])
+	end
+
+	private 
+
+	def is_allowed
+		unless current_user.is_admin
+			flash[:alert] = "You are not allowed here"
+			redirect_to root_path
+		end
 	end
 end
