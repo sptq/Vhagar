9352f4c4036fa720b4011cae6baa22d92eb2e378
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-03-01 18:15:25 +0100

    Friendly login/register/etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Crm::Application.routes.draw do
-  devise_for :users
+
+  devise_for :user, :path => '', path_names: { 
+    sign_in: "login",
+    sign_out: "logout",
+    sign_up: "register",
+    password: 'secret',
+    confirmation: 'verification',
+    edit: 'edit/profile'
+  }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
