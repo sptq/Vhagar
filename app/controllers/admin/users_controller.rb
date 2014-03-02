f3d6051edcd1bc4115d6b6fe7dfc15c324e9a05a
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-03-02 22:51:50 +0100

    Ability to edit and update user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,25 @@
 class Admin::UsersController < ApplicationController
 
 	before_filter :authenticate_user!, :is_allowed
+	before_action :set_user, only: [:edit, :update]
 
 	def index
 		@users = User.all
 	end
 
 	def edit
-		@user = User.where(uuid: params[:id]).first
+	end
+
+	def update
+		respond_to do |format|
+			if @user.update(user_params)
+				format.html { redirect_to users_path, notice: 'User was successfully updated. '}
+				format.json { head :no_content}
+			else
+				format.html { render action: :edit }
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+			end
+		end
 	end
 
 	private
@@ -17,5 +29,15 @@ class Admin::UsersController < ApplicationController
 			flash[:alert] = "You are not allowed here"
 			redirect_to root_path
 		end
+	end
+
+    # Use callbacks to share common setup or constraints between actions.
+	def set_user
+		@user = User.where(uuid: params[:id]).first
+	end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+	def user_params
+		params.require(:user).permit(:user_role, :type)
 	end
 end
