7b103f4fbbb12dcb457f77e35707f18a8a6900d0
sptq <sptq@hslbn.pl>
2014-03-07 09:45:32 +0100

    Fix migration and connect profile with user

--- db/migrate/20140302215610_create_profiles.rb
+++ db/migrate/20140302215610_create_profiles.rb
@@ -1,14 +1,14 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
-    create_table :profiles do |t|
+    create_table :profiles, id: :uuid do |t|
       t.string :firstName,      :null => false
-      t.string :lastname,       :null => false
+      t.string :lastName,       :null => false
       t.string :job,            :default => ""
       t.string :place,          :default => ""
       t.string :phone,          :default => ""
 
       # Assosations
-      t.belongs_to :user
+      t.uuid :user_id
 
       t.timestamps
     end
