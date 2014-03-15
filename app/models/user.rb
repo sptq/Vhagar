929962c78ebad3d68b4d25e14abc79db1b7e0bea
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-03-15 17:23:18 +0100

    Fix ensure admin presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,26 +9,19 @@ class User < ActiveRecord::Base
 
   validates :uuid, presence: true
   validates :user_role, presence: true
-  before_validation :ensure_uuid
-  after_save :ensure_admin_presence
+  before_validation :ensure_uuid, :ensure_admin_presence
 
-  def ensure_uuid; self.uuid ||= SecureRandom.uuid end
+  def ensure_uuid
+    self.uuid ||= SecureRandom.uuid
+  end
+  def ensure_admin_presence
+    self.user_role = 'admin' if User.where(user_role: 'admin').to_a.count == 0
+  end
 
   # named_scope :with_role, lambda { |role| {:conditions => "roles_mask & #{2**ROLES.index(role.to_s)} > 0 "} }
   ROLES = %w[admin staff user guest]
 
   def role?(role)
     user_role.to_s.include? role.to_s
-  end
-
-  def admin?
-    role? :admin
-  end
-
-private
-
-  def ensure_admin_presence
-    @admins = User.where(['user_role LIKE ?', '%admin%']).to_a
-    user_role = 'admin' unless @admins.count > 0
   end
 end
