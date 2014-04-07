cd920c3c5e25037d352fb5e916393f7c753a195e
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-04-07 21:01:52 +0200

    Add reserved places to lecture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407164330) do
+ActiveRecord::Schema.define(version: 20140407184231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140407164330) do
     t.datetime "updated_at"
     t.uuid     "room_id"
     t.integer  "duration",    default: 30, null: false
+    t.integer  "reserved",    default: 0,  null: false
   end
 
   create_table "participations", force: true do |t|
