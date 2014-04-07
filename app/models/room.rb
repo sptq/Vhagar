6eac3e95f4efc49fafdea7048c27ea0d5356a938
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-04-07 02:41:23 +0200

    Minor fix with attending event

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
+  validates :capacity, presence: true
   before_validation :ensure_uuid
 
   def ensure_uuid; self.id ||= SecureRandom.uuid end
