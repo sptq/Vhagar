91d0d66ec0c680ad8b215e9e72295dccca7f0c3e
sptq <sptq@hslbn.pl>
2014-04-07 15:31:57 +0200

    Fix error form field

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Crm::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+  html_tag
+end
