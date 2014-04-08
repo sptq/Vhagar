6a129add7322230f4d2b6652319cedf995f21df7
sptq <sptq@hslbn.pl>
2014-04-08 17:24:07 +0200

    Add simple stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,16 +3,16 @@ class StatsController < ApplicationController
 	load_and_authorize_resource
 
 	def index
-		allUsers = User.all.count
-		unapprovedUsers = User.where(confirmed_at: nil).count
-		approvedUsers = allUsers = unapprovedUsers;
-		usersWithTickets = User.where(ztmTicket: true).count
+		@users = User.all.count
+		@unapprovedUsers = User.where(confirmed_at: nil).count
+		@approvedUsers = @users - @unapprovedUsers;
+		@usersWithTickets = User.where(ztmTicket: true).count
 
 		@users = {
-			:all => allUsers,
-			:approvedUsers => approvedUsers,
-			:unapproved => unapprovedUsers,
-			:withTickets => usersWithTickets
+			:all => @users,
+			:approvedUsers => @approvedUsers,
+			:unapproved => @unapprovedUsers,
+			:withTickets => @usersWithTickets
 		}
 	end
 end
