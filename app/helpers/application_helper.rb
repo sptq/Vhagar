cddf2de8f2a166dad49ee84a6cfc30d35d24264b
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-04-09 18:12:27 +0200

    Gravatar image in user profile closes #6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,18 @@
 module ApplicationHelper
-  def title(page_title)
-    content_for(:title) { page_title }
-  end
+	def title(page_title)
+		content_for(:title) { page_title }
+	end
+
+	def avatar_url(user)
+		if defined? user.avatar_url and user.avatar_url.present?
+			user.avatar_url
+		else
+			gravatar_url(user)
+		end
+	end
+
+	def gravatar_url(user)
+		gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+		"http://gravatar.com/avatar/#{gravatar_id}"
+	end
 end
