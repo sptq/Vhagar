1259d38507a24853d43b7f06a2657ced7a486948
sptq <sptq@hslbn.pl>
2014-04-24 07:37:51 +0200

    Fix premission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,9 +33,6 @@ class ApplicationController < ActionController::Base
 
 	def configure_permitted_parameters
   		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :acceptMarketingTerms, :acceptTerms, :email, :password, :password_confirmation) }
-	end
-
-	def configure_permitted_parameters
-  		devise_parameter_sanitizer.for(:account_update) { |u| u.permit({ roles: [] }, :acceptMarketingTerms, :acceptTerms, :email, :password, :password_confirmation, :current_password) }
+		devise_parameter_sanitizer.for(:account_update) { |u| u.permit({ roles: [] }, :acceptMarketingTerms, :acceptTerms, :email, :password, :password_confirmation, :current_password) }
 	end
 end
