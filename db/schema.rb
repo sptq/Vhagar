83d42e104a2be215024fc7357ede46f24ee04eb8
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-04-26 23:26:32 +0200

    Confirm user participation in lecture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140426130503) do
+ActiveRecord::Schema.define(version: 20140426205955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20140426130503) do
     t.uuid     "lecture_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "confirmed_at"
   end
 
   create_table "profiles", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
